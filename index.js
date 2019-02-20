0c7e0ad64f68608a9a5a22ed09c4afcc1a1e9f53
Aidan Kelley <aidanmkelley@gmail.com>
2019-02-19 18:36:11 -0600

    Made it gather less data (no longer records which keys you press)

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const keylogger = require('osx-keylogger'),
 // shows only the stuff from the keylogger(not stdin) but breaks ctrl+c
 // process.stdin.setRawMode(true);
 
+let file = "output/output_" + Date.now() + ".csv";
 
+if(process.argv.indexOf("-l") >= 0) {
+    file = "output/combined_log.csv";
+}
 
-let stream = fs.createWriteStream("output/output_" + Date.now() + ".txt");
+let stream = fs.createWriteStream(file, {flags:'a'});
 
 let currentModifiers = '';
 keylogger.listen((modifiers, key) => {
-    stream.write(Date.now() + "," + JSON.stringify(key) +"\n");
+    stream.write(Date.now() + "\n");
 }, 'english.json');
